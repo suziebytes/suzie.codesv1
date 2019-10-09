81681995bbd126b3c188eee7c020cd3ad6d75fcf
Suzie <reorsusie@gmail.com>
2019-10-09 13:55:14 -0700

    +styled components for card

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,14 +4,19 @@ import styled from "styled-components";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import CardAbout from "./CardAbout";
 
 export default function AboutMe() {
   return (
     <Background>
       <Container>
         <Row>
-          <Col lg="7"></Col>
-          <Col lg="5"></Col>
+          <Col lg="5">
+            <Wrapper>
+              <CardAbout />
+            </Wrapper>
+          </Col>
+          <Col lg="8"></Col>
         </Row>
       </Container>
     </Background>
@@ -24,5 +29,10 @@ const Background = styled.div`
   width: 100%;
   background-size: cover;
   background-repeat: no-repeat;
-  background-color: #f2eeef;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
 `;
--- src/Components/AboutMe/CardAbout.js
+++ src/Components/AboutMe/CardAbout.js
@@ -3,32 +3,39 @@ import React from "react";
 // import styles from "./styles";
 import aboutimg from "./aboutimg.jpg";
 import styled from "styled-components";
-import { Card, Icon, Image } from "semantic-ui-react";
+import Card from "react-bootstrap/Card";
 
 export default function CardAbout() {
   return (
     <Card>
-      <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Front End Developer</Card.Header>
-        <Card.Meta>
-          <span className="date">& Web Designer</span>
-        </Card.Meta>
-        <Card.Description>
-          I merge my creative design background with my technical experience to
-          build eye catching & functional products for my clients
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
+      <Wrapper>
+        <CardTitle>FRONT END DEVELOPER</CardTitle>
+        <CardText>
+          I merged my creative design background with my technical experience to
+          build eye catching & functional products for my clients.
+        </CardText>
+      </Wrapper>
     </Card>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 15px;
+`;
+
+const CardTitle = styled.h2`
+  font-size: 13pt;
+  font-weight: 500;
+  letter-spacing: 2px;
+  color: #388e71;
+  text-align: center;
+  padding: 20px 0 20px 0;
+`;
+
+const CardText = styled.p`
+  font-size: 9pt;
+  font-weight: 200;
+  letter-spacing: 1.5px;
+  color: #707070;
+  text-align: left;
+`;
