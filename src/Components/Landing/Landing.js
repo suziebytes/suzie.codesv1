45d224059e9621942a35ecfb6a1ef9843418b41b
Suzie <reorsusie@gmail.com>
2019-10-09 00:40:51 -0700

    removed focus outline on buttons

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -10,6 +10,7 @@ import Container from "react-bootstrap/Container";
 function Landing() {
   return (
     <Background>
+      <ContactButton>Let's Talk</ContactButton>
       <Container>
         <Row>
           <Col lg="7"></Col>
@@ -37,7 +38,6 @@ const Background = styled.div`
   width: 100%;
   background-size: cover;
   background-repeat: no-repeat;
-  display: flex;
   justify-content: left;
   background-color: #f2eeef;
   position: absolute;
@@ -47,14 +47,13 @@ const Background = styled.div`
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 235px;
 `;
 
 const Welcome = styled.h1`
   font-size: 45pt !important;
   color: #707070;
   font-weight: 200;
-  margin-top: 80%;
+  margin-top: 60%;
   margin-bottom: 0 !important;
 `;
 
@@ -68,10 +67,38 @@ const Tag = styled.span`
 
 const Button = styled.button`
   font-size: 9pt;
-  padding: 5px 45px 5px 45px;
+  color: #707070;
+  padding: 5px 0 5px 0;
   border: 1px solid #fcc8c8;
   background: rgba(255, 255, 255, 0.6);
   border: 1px solid #fcc8c8;
+  width: 150px;
+  display: inline-block;
+  vertical-align: middle;
+  transform: translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  backface-visibility: hidden;
+  -moz-osx-font-smoothing: grayscale;
+  transition-duration: 0.3s;
+  transition-property: transform;
   &: hover {
+    transform: scale(1.05);
+  }
+  &: focus {
+    outline: none;
+  }
+`;
+
+const ContactButton = styled(Button)`
+  background: none;
+  color: #707070;
+  padding: 5px 15px 5px 15px;
+  font-size: 10pt;
+  border-radius: 8px 8px 8px 8px;
+  border: 1.5px solid #fcc8c8;
+  margin: 10px 15px 0 0;
+  float: right;
+  &:hover {
+    transform: scale(0.9);
   }
 `;
