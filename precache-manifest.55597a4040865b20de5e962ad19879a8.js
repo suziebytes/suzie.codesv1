0a849834acca0985da70b30ae955477faba0676e
Suzie <32041993+suziebytes@users.noreply.github.com>
2022-10-26 23:35:59 -0700

    Updates

---
+++ precache-manifest.55597a4040865b20de5e962ad19879a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea017a349f28e4b32cd7241f2e5c73a",
+    "url": "https://suziebytes.github.io/suzie.codesv1/index.html"
+  },
+  {
+    "revision": "82b60f7646b62d8590b2",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/css/main.80086952.chunk.css"
+  },
+  {
+    "revision": "e255d3832daea7c50c66",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/js/2.d452005f.chunk.js"
+  },
+  {
+    "revision": "82b60f7646b62d8590b2",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/js/main.686cd6dd.chunk.js"
+  },
+  {
+    "revision": "02ada645be543714e58f",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/js/runtime-main.da896ff1.js"
+  },
+  {
+    "revision": "7ed3c1eda50df885a11eddf670120a46",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/aboutimg.7ed3c1ed.jpg"
+  },
+  {
+    "revision": "4eb43c91190611736d580662753c6e32",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/bg.4eb43c91.png"
+  },
+  {
+    "revision": "5b443bbd12bd968b2a7cba787dc3c624",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/bgimg.5b443bbd.jpg"
+  },
+  {
+    "revision": "b63c8186e7bd005a099e8595bb0dfeb0",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/desktop.b63c8186.png"
+  },
+  {
+    "revision": "cee4eb5907aae8019b13ee9d0cbc3a3b",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/graphic.cee4eb59.png"
+  },
+  {
+    "revision": "f7bbd086a80380491cf3727580d1e284",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/main.f7bbd086.jpg"
+  },
+  {
+    "revision": "c4291f125291e6c6f65130b9eaee476d",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/self.c4291f12.png"
+  },
+  {
+    "revision": "70057462c3868bba68ef46a83096e8e5",
+    "url": "https://suziebytes.github.io/suzie.codesv1/static/media/suzbg.70057462.png"
+  }
+]);
